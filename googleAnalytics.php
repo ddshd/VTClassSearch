d02ddde61a6f58fa7ce868f49294237a67f85404
Dhrumil Shah <ddshd@users.noreply.github.com>
2024-04-12 00:50:15 -0400

    Add Google Analytics

---
+++ googleAnalytics.php
@@ -0,0 +1,31 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113584391-2"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-113584391-2');
+</script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPHMPXZ');</script>
+<!-- End Google Tag Manager -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPHMPXZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<script type="application/javascript">
+  function getIP(json) {
+    var ip = json.ip;
+    dataLayer.push({"event":"ipEvent","ipAddress" : json.ip});
+  }
+</script>
+<script type="application/javascript" src="https://api.ipify.org/?format=jsonp&callback=getIP"></script>
+
+<meta name="viewport" content="width=device-width">
